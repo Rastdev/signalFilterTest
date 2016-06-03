76155750a67c6455634ed88218c973064049870e
vaio <bsuirmail@gmail.com>
2016-06-03 15:32:37 +0300

    Fix bug: refillRate variable typecasting

--- src/main/java/test/filter/impl/RateFilter.java
+++ src/main/java/test/filter/impl/RateFilter.java
@@ -13,27 +13,31 @@ public class RateFilter implements Filter {
 
     public RateFilter(int ratePerUnit, TimeUnit timeUnit) {
         this.maxCapacity = ratePerUnit;
-        refillRate = ratePerUnit / timeUnit.toNanos(1);
+        refillRate = ratePerUnit / (double) timeUnit.toNanos(1);
         lastAcceptRef = new AtomicStampedReference<Long>(0L, Float.floatToIntBits(this.maxCapacity));
     }
 
     @Override
     public boolean isSignalAllowed() {
         boolean compareAndSetHappen;
+        long elapsedTime;
+        double allowance;
         do {
             long now = System.nanoTime();
             Long lastAcceptTime = lastAcceptRef.getReference();
             int prevStamp = lastAcceptRef.getStamp();
-            long elapsedTime = now - lastAcceptTime;
-            double allowance = Float.intBitsToFloat(prevStamp);
+            elapsedTime = now - lastAcceptTime;
+            allowance = Float.intBitsToFloat(prevStamp);
             allowance += elapsedTime * refillRate;
             allowance = Math.min(allowance, maxCapacity);
             if (allowance < 1.0) {
+                System.out.println("Discard, elapsed time: " + elapsedTime / 1000000 + " allowance: " + allowance);
                 return false;
             }
             int newStamp = Float.floatToIntBits((float) (allowance - 1.0));
             compareAndSetHappen = lastAcceptRef.compareAndSet(lastAcceptTime, now, prevStamp, newStamp);
         } while (!compareAndSetHappen);
+        System.out.println("Allow, elapsed time: " + elapsedTime / 1000000 + " allowance: " + allowance);
         return true;
     }
 }
